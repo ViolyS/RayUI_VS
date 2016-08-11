54e29ffe070b5b2e9dd739ebac34ac10b5b0c294
ViolyS <386949142@qq.com>
2016-08-11 11:42:42 +0800

    DH适配

--- Interface/AddOns/RayUI/core/consts.lua
+++ Interface/AddOns/RayUI/core/consts.lua
@@ -10,29 +10,29 @@ BINDING_HEADER_RAYUI = GetAddOnMetadata(AddOnName, "Title")
 RayUF.colors.power["MANA"] = {0, 0.8, 1}
 
 RayUF["colors"].class = {
-	["DEATHKNIGHT"] = { 0.77,	0.12,		0.23 },
-	["DRUID"]       = { 1,		0.49,		0.04 },
-	["HUNTER"]      = { 0.58,	0.86,		0.49 },
-	["MAGE"]        = { 0.2,	0.76,		1 },
-	["PALADIN"]     = { 1,		0.42,		0.62 },
-	["PRIEST"]      = { 1,		1,			1 },
-	["ROGUE"]       = { 1,		0.91,		0.3 },
-	["SHAMAN"]      = { 0.16,	0.31,		0.61 },
-	["WARLOCK"]     = { 0.6,	0.47,		0.85 },
-	["WARRIOR"]     = { 0.9,	0.65,		0.45 },
-	["MONK"]        = { 0,		1,			0.59 },
-	["DEMONHUNTER"]     = { 0.64,	0.19,		0.39 },
+	["DEATHKNIGHT"] = { 0.77, 0.12, 0.23}, -- #c41f3b
+	["DEMONHUNTER"] = { 0.64, 0.19, 0.79}, -- #a330c9
+	["DRUID"] = { 1, 0.49, 0.04}, -- #ff7d0a
+	["HUNTER"] = { 0.67, 0.83, 0.45}, -- #abd473
+	["MAGE"] = { 0.25, 0.78, 0.92}, -- #3fc7eb
+	["MONK"]	 = { 0, 1, 0.59}, -- #00ff96
+	["PALADIN"] = { 0.96, 0.55, 0.75}, -- #f58cba
+	["PRIEST"] = { 1, 1, 1}, -- #ffffff
+	["ROGUE"] = { 1, 0.96, 0.41}, -- #fff569
+	["SHAMAN"] = { 0, 0.44, 0.87}, -- #0070de
+	["WARLOCK"] = { 0.52, 0.52, 0.93}, -- #8788ee
+	["WARRIOR"] = { 0.78, 0.61, 0.43}, -- #c79c6e
 }
 
 RayUF["colors"].reaction = {
-	[1] = {1, 0.2, 0.2}, -- Hated
-	[2] = {1, 0.2, 0.2}, -- Hostile
-	[3] = {1, 0.6, 0.2}, -- Unfriendly
-	[4] = {1, 1, 0.2}, -- Neutral
-	[5] = {0.2, 1, 0.2}, -- Friendly
-	[6] = {0.2, 1, 0.2}, -- Honored
-	[7] = {0.2, 1, 0.2}, -- Revered
-	[8] = {0.2, 1, 0.2}, -- Exalted
+	[1] = {1,	0.2,	0.2}, -- Hated
+	[2] = {1,	0.2,	0.2}, -- Hostile
+	[3] = {1,	0.6,	0.2}, -- Unfriendly
+	[4] = {1,	1,	0.2}, -- Neutral
+	[5] = {0.2,	1,	0.2}, -- Friendly
+	[6] = {0.2,	1,	0.2}, -- Honored
+	[7] = {0.2,	1,	0.2}, -- Revered
+	[8] = {0.2,	1,	0.2}, -- Exalted
 }
 
 RayUF["colors"].ComboPoints = {
