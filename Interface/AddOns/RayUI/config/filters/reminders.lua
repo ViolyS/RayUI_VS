ab3fd24908e2350949d5aa6e79b59b7d66aa847c
ViolyS <386949142@qq.com>
2017-01-02 14:10:00 +0800

    update

--- Interface/AddOns/RayUI/config/filters/reminders.lua
+++ Interface/AddOns/RayUI/config/filters/reminders.lua
@@ -3,30 +3,42 @@
 P["Reminder"] = {}
 
 P["Reminder"]["filters"] = {
-	PALADIN = {
-		["强效祝福"] = {
-			["spellGroup"] = {
-				[203528] = true, -- 强效力量祝福
-				[203538] = true, -- 强效王者祝福
-				[203539] = true, -- 强效智慧祝福
-				["defaultIcon"] = 203528
-			},
-			["enable"] = true,
-			["strictFilter"] = true,
-			["tree"] = 3,
-		}
-	},
-	ROGUE = {
-		["伤害性毒药"] = {
-			["spellGroup"] = {
-				[200802] = true, -- 苦痛毒藥
-				[8679] = true, -- 致傷毒藥
-				[2823] = true, -- 致命毒藥
-				["defaultIcon"] = 76803
-			},
-			["enable"] = true,
-			["strictFilter"] = true,
-			["tree"] = 1,
-		}
-	},
+    PALADIN = {
+        ["强效祝福"] = {
+            ["spellGroup"] = {
+                [203528] = true, -- 强效力量祝福
+                [203538] = true, -- 强效王者祝福
+                [203539] = true, -- 强效智慧祝福
+                ["defaultIcon"] = 203528
+            },
+            ["enable"] = true,
+            ["strictFilter"] = true,
+            ["tree"] = 3,
+        }
+    },
+    ROGUE = {
+        ["伤害性毒药"] = {
+            ["spellGroup"] = {
+                [200802] = true, -- 苦痛毒藥
+                [8679] = true, -- 致傷毒藥
+                [2823] = true, -- 致命毒藥
+                ["defaultIcon"] = 76803
+            },
+            ["enable"] = true,
+            ["strictFilter"] = true,
+            ["tree"] = 1,
+        }
+    },
+    
+    MAGE = {
+        ["奥术魔宠"] = {
+            ["spellGroup"] = {
+                [210126] = true,
+                ["defaultIcon"] = 210126
+            },
+            ["enable"] = true,
+            ["strictFilter"] = true,
+            ["tree"] = 1,
+        }
+    },
 }
