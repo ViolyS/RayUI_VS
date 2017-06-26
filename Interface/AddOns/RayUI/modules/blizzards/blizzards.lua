f7fd3e2823f1ffa65cb43b5f0abaebc72d0b69f5
ViolyS <386949142@qq.com>
2017-06-26 17:09:38 +0800

    sync/m/bliz

--- Interface/AddOns/RayUI/modules/blizzards/blizzards.lua
+++ Interface/AddOns/RayUI/modules/blizzards/blizzards.lua
@@ -1,18 +1,18 @@
---AlertSystem from ls: Toasts
 ----------------------------------------------------------
 -- Load RayUI Environment
 ----------------------------------------------------------
-_LoadRayUIEnv_()
+RayUI:LoadEnv("Blizzards")
 
 
 local B = R:NewModule("Blizzards", "AceEvent-3.0", "AceHook-3.0", "AceConsole-3.0")
+_Blizzards = B
 
 function B:Initialize()
     CreateFrame("Frame"):SetScript("OnUpdate", function(self, elapsed)
-        if LFRBrowseFrame.timeToClear then
-        LFRBrowseFrame.timeToClear = nil
-        end
-    end)
+		if LFRBrowseFrame.timeToClear then
+			LFRBrowseFrame.timeToClear = nil
+		end
+	end)
 end
 
 R:RegisterModule(B:GetName())
