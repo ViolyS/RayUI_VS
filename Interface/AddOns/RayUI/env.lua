0f40ba2410686acaff4f982d37724a9f5c721e63
ViolyS <386949142@qq.com>
2017-06-26 14:26:54 +0800

    fix buffs

--- Interface/AddOns/RayUI/env.lua
+++ Interface/AddOns/RayUI/env.lua
@@ -1,21 +1,26 @@
 do
     local _G, rawset    = _G, rawset
+    local _OrigEnv      = getfenv(1)
     local _RayUIEnv     = setmetatable({}, {
         __index         = function(self, k)
-			local v = _G[k]
-			if v ~= nil then
-				rawset(self, k, v)
-				return v
-			end
-		end,
+            local v = _G[k]
+            if v ~= nil then
+                rawset(self, k, v)
+                return v
+            end
+        end,
         __metatable     = true,
     })
     _RayUIEnv._RayUIEnv = _RayUIEnv
     _RayUIEnv._AddOnName = "RayUI"
 
-	_LoadRayUIEnv_ = function()
-		setfenv(2, _RayUIEnv)
-	end
+    _LoadRayUIEnv_ = function()
+        setfenv(2, _RayUIEnv)
+    end
+
+    _LoadDefaultEnv_ = function()
+        setfenv(2, _OrigEnv)
+    end
 
     setfenv(1, _RayUIEnv)
 end
